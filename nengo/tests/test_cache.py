273ea35b558009277efb065bf8bd8321a6b3357c
Jan Gosmann <jan@hyper-world.de>
2014-05-02 17:35:32 -0400

    Test caching with E argument to solver.

--- nengo/tests/test_cache.py
+++ nengo/tests/test_cache.py
@@ -28,7 +28,7 @@ class DecoderSolverMock(object):
             if E is None:
                 return np.random.rand(A.shape[1], Y.shape[1]), {'info': 'v'}
             else:
-                return np.random.rand(A.shape[1], E.shape[2]), {'info': 'v'}
+                return np.random.rand(A.shape[1], E.shape[1]), {'info': 'v'}
         solver_fn.__name__ = self.name
         return solver_fn
 
@@ -43,6 +43,7 @@ def test_decoder_cache(cache_dir):
     targets = np.ones((M, N))
     rng = np.random.RandomState(42)
 
+    # Basic test, that results are cached.
     cache = DecoderCache(cache_dir)
     decoders1, solver_info1 = cache(solver_mock.get_solver_fn())(
         activities, targets, rng)
@@ -53,8 +54,35 @@ def test_decoder_cache(cache_dir):
     assert_equal(decoders1, decoders2)
     assert solver_info1 == solver_info2
 
+    decoders3, solver_info3 = cache(solver_mock.get_solver_fn())(
+        2 * activities, targets, rng)
+    assert solver_mock.n_calls == 2
+    assert np.any(decoders1 != decoders3)
+
+    # Test that the cache does not load results of another solver.
     another_solver = DecoderSolverMock('another_solver')
     cache(another_solver.get_solver_fn())(activities, targets, rng)
     assert another_solver.n_calls == 1
 
-    # TODO test using E
+
+def test_decoder_cache_with_E_argument_to_solver(cache_dir):
+    solver_mock = DecoderSolverMock()
+
+    M = 100
+    N = 10
+    N2 = 5
+    D = 2
+    activities = np.ones((M, D))
+    targets = np.ones((M, N))
+    rng = np.random.RandomState(42)
+    E = np.ones((D, N2))
+
+    cache = DecoderCache(cache_dir)
+    decoders1, solver_info1 = cache(solver_mock.get_solver_fn())(
+        activities, targets, rng, E=E)
+    assert solver_mock.n_calls == 1
+    decoders2, solver_info2 = cache(solver_mock.get_solver_fn())(
+        activities, targets, rng, E=E)
+    assert solver_mock.n_calls == 1  # check the result is read from cache
+    assert_equal(decoders1, decoders2)
+    assert solver_info1 == solver_info2
