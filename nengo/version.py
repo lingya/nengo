6d72b4a5e40cccd8349544577f5a75978382c71b
Jan Gosmann <jan@hyper-world.de>
2014-05-03 19:36:45 -0400

    Add name and author info to version module.

--- nengo/version.py
+++ nengo/version.py
@@ -21,3 +21,6 @@ dev = True
 version = "{0}{1}{2}".format('.'.join(str(v) for v in version_info),
                              '-rc{0:d}'.format(rc) if rc is not None else '',
                              '-dev' if dev else '')
+
+name = "nengo"
+author = "CNRGlab at UWaterloo"
