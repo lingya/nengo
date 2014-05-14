de5923abf7bed8a81ef278bb8c87d16a4b181b65
Jan Gosmann <jgosmann@uwaterloo.ca>
2014-05-14 13:37:03 -0400

    Add documentation.

--- nengo/runcom.py
+++ nengo/runcom.py
@@ -1,3 +1,30 @@
+r"""This modules provides access to the nengo RC settings.
+
+Nengo RC settings will be read from the following files in the given order:
+1. `INSTALL/nengo-data/nengorc`  (where INSTALL is the installation directory
+    of the Nengo package)
+2. A operating system specific file in the user's home directory.
+   Linux: `~/.config/nengo/nengorc`
+   OS X: `~/Library/Application Support/nengo/nengorc`
+   Windows 7: `%userprofile%\AppData\Local\CNRGlab at UWaterloo\nengo`
+3. File given in the environment variable ``$NENGORC``.
+4. nengorc in the current directory.
+
+
+The RC file is divided into sections by lines containing the section name
+in brackets, i.e. ``[section]``. A setting is set by giving the name followed
+by a ``:`` or ``=`` and the value. All lines starting with ``#`` or ``;`` are
+comments.
+
+Example
+-------
+
+This example demonstrates how to set settings in an RC file:
+
+    [decoder_cache]
+    size: 536870912  # setting the decoder cache size to 512MiB.
+"""
+
 try:
     import configparser
 except ImportError:
@@ -19,6 +46,8 @@ DEFAULTS = {
         'path': os.path.join(_APPDIRS.user_cache_dir, 'decoders')
     }
 }
+"""The default core Nengo RC settings. Access with
+``DEFAULTS[section_name][option_name]``."""
 
 DEFAULT_RC_FILES = [
     os.path.abspath(os.path.join(
@@ -27,9 +56,12 @@ DEFAULT_RC_FILES = [
     os.environ['NENGORC'] if 'NENGORC' in os.environ else '',
     os.path.join(os.curdir, 'nengorc')
 ]
+"""The RC files in the order in which they will be read."""
 
 
 class _Runcom(configparser.SafeConfigParser):
+    """Allows to read and write Nengo RC settings."""
+
     def __init__(self):
         # configparser uses old-style classes without 'super' support
         configparser.SafeConfigParser.__init__(self)
@@ -47,6 +79,13 @@ class _Runcom(configparser.SafeConfigParser):
                     self.set(section, k, repr(v))
 
     def reload_rc(self, filenames=None):
+        """Resets the currently loaded RC settings and loads new RC files.
+
+        Parameters
+        ----------
+        filenames: iterable object
+            Filenames of RC files to load.
+        """
         if filenames is None:
             filenames = DEFAULT_RC_FILES
 
@@ -56,3 +95,4 @@ class _Runcom(configparser.SafeConfigParser):
 
 
 runcom = _Runcom()
+"""The current Nengo RC settings."""
