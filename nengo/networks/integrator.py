e15c775f0068727bd4a48a5125e3b392500f880f
Sean Aubin <saubin@uwaterloo.ca>
2014-10-15 18:24:12 -0400

    replaced reserved keywords

--- nengo/networks/integrator.py
+++ nengo/networks/integrator.py
@@ -5,8 +5,8 @@ def Integrator(recurrent_tau, n_neurons, dimensions, net=None):
     if net is None:
         net = nengo.Network(label="Integrator")
     with net:
-        net.input = nengo.Node(size_in=dimensions)
+        net.integrator_input = nengo.Node(size_in=dimensions)
         net.ensemble = nengo.Ensemble(n_neurons, dimensions=dimensions)
         nengo.Connection(net.ensemble, net.ensemble, synapse=recurrent_tau)
-        nengo.Connection(net.input, net.ensemble, transform=recurrent_tau)
+        nengo.Connection(net.integrator_input, net.ensemble, transform=recurrent_tau)
     return net
--- nengo/networks/oscillator.py
+++ nengo/networks/oscillator.py
@@ -5,12 +5,12 @@ def Oscillator(recurrent_tau, frequency, n_neurons, net=None):
     if net is None:
         net = nengo.Network(label="Oscillator")
     with net:
-        net.input = nengo.Node(label="In", size_in=2)
+        net.oscillator_input = nengo.Node(label="In", size_in=2)
         net.ensemble = nengo.Ensemble(
             n_neurons, dimensions=2, label="Oscillator")
         tA = [[1, -frequency * recurrent_tau],
               [frequency * recurrent_tau, 1]]
         nengo.Connection(net.ensemble, net.ensemble,
                          synapse=recurrent_tau, transform=tA)
-        nengo.Connection(net.input, net.ensemble)
+        nengo.Connection(net.oscillator_input, net.ensemble)
     return net
